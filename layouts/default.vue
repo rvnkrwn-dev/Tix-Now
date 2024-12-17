53cfacac5bc0dce9bcfc65957d5daa38900f7446
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-12-17 15:44:39 +0700

    update and chore

--- layouts/default.vue
+++ layouts/default.vue
@@ -91,7 +91,18 @@
           <div
               class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
             <div class="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
-              <NuxtLink class="p-2 flex items-center text-sm text-orange-600 focus:outline-none focus:text-orange-600"
+              <NuxtLink active-class="text-orange-600" class="p-2 flex items-center text-sm focus:outline-none focus:text-orange-600"
+                        to="/" aria-current="page">
+                <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24"
+                     height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                     stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+                  <path
+                      d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                </svg>
+                Landing
+              </NuxtLink>
+              <NuxtLink active-class="text-orange-600" class="p-2 flex items-center text-sm focus:outline-none focus:text-orange-600"
                         to="/events" aria-current="page">
                 <svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden" xmlns="http://www.w3.org/2000/svg" width="24"
                      height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
