52126431f5d1f2eace596ac775f405fd8656683c
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-12-17 16:01:28 +0700

    update and chore

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -378,20 +378,6 @@
                     <li>
                       <NuxtLink
                           class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                          to="/admin/transactions/add" active-class="bg-gray-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round" class="lucide lucide-circle-plus">
-                          <circle cx="12" cy="12" r="10"/>
-                          <path d="M8 12h8"/>
-                          <path d="M12 8v8"/>
-                        </svg>
-                        Tambah
-                      </NuxtLink>
-                    </li>
-                    <li>
-                      <NuxtLink
-                          class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                           to="/admin/transactions/edit" active-class="bg-gray-100">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"
                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
